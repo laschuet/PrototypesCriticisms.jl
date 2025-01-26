d99ff1a20208a1bfd6c8cf0993c380d08c4ae32d
laschuet <laschuet@users.noreply.github.com>
2025-01-26 22:09:30 +0100

    Move variables to correct scope

--- test/ranking.jl
+++ test/ranking.jl
@@ -1,9 +1,9 @@
 @testset "ranking" begin
     observed_ranking = [1, 2, 3, 4, 5, 6]
+    ranking_1 = [2, 1, 3, 4, 6, 5]
+    ranking_2 = [6, 2, 5, 3, 4, 1]
 
     @testset "importances" begin
-        ranking_1 = [2, 1, 3, 4, 6, 5]
-        ranking_2 = [6, 2, 5, 3, 4, 1]
         expected_importances = [52 // 21, 92 // 21, 89 // 21, 97 // 21, 84 // 21, 52 // 21]
         computed_importances = importances(observed_ranking, [ranking_1 ranking_2])
         @test isapprox(expected_importances, computed_importances)
