b17ff46791c32de12850d57c39035b9bd77201d2
laschuet <laschuet@users.noreply.github.com>
2025-02-05 17:46:54 +0100

    Change type

--- src/ranking.jl
+++ src/ranking.jl
@@ -51,10 +51,10 @@ Find prototypes and criticisms based on the clustering of the ranking.
 
 # Arguments
 - `observed_ranking::Vector{Int}`: the observed ranking.
-- `D::Matrix`: the data instances.
+- `D::AbstractArray`: the data instances.
 - `k::Int`: the number of clusters to find.
 """
-function prototypes_criticisms(observed_ranking::Vector{Int}, D::Matrix, k::Int)
+function prototypes_criticisms(observed_ranking::Vector{Int}, D::AbstractArray, k::Int)
     length(observed_ranking) > size(D, 2) && throw(
         ArgumentError(
             "Number of data instances in the ranking must be lesser or equal to the number of data instances in general",
